f31cae2a54d5beebe900772573179b4fa62ffa1c
mahesa-flo <mahesa@floridauniversitaria.es>
2021-11-25 20:39:18 +0100

    subo Fighter y GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject fighter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Fighter luchador = fighter.GetComponent<Fighter>();
+
+
+        //Debug.Log("[GM] el nombre del luchador es: " + fighter.GetComponent<Fighter>().nombre);
+        //Debug.Log("[GM] la categoría es: "+ fighter.GetComponent<Fighter>().category);
+        //Debug.Log("[GM] la vida es: " + fighter.GetComponent<Fighter>().health);
+
+
+        Debug.Log("[GM] el nombre del luchador es: " + luchador.nombre);
+        Debug.Log("[GM] la categoría es: " + luchador.category);
+        Debug.Log("[GM] la vida es: " + luchador.health);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
