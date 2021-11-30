bac4ce81712c649c071874c6d5ee1273f0f91c6d
mahesa-flo <mahesa@floridauniversitaria.es>
2021-11-30 12:40:42 +0100

    actualizo Fighter.cs y GameManager.cs

--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -1,26 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Fighter : MonoBehaviour
 {
+    Fighter luchador;
+
+    public string nombre;
+    public string category;
+    public int health;
+
+    public Fighter(string nombre, string category, int healt)
+    {
+        this.nombre = nombre;
+        this.category = category;
+        this.health = healt;
+    }
+
+    //muestra los datos del Fighter
+    public string MuestraInfoLuchador()
+    {
+        return ("Nombre: " + this.nombre + " / Categoria: " + this.category + " / Vida: " + this.health);
+    }
+
+    //quita vida al Fighter
+    public void DecreaseHealth(int quitarVida)
+    {
+        this.health -= quitarVida;
+    }
+
+    //consulta si un Fighter está vivo
+    public bool IsAlive()
+    {
+        bool estaVivo;
+        if (this.luchador.health > 0) estaVivo = true;
+        else estaVivo = false;
+
+        return estaVivo;
+    }
 
 
-    public string nombre = "Scorpion";
-    public string category = "Ninja";
-    public int health = 95;
+
+    // OJO!!! revisar esto....
+    //EJERCICIO 12:
+    //cuando el Fighter muere notifica al GO que ha muerto
+    //public Fighter NotificaMuerto() {
+
+    //if(this.health==0) return luchador;
+    //}
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,30 +1,159 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 
-    public GameObject fighter;
+    public GameObject fighter1;
+    public GameObject fighter2;
+    public GameObject fighter3;
+    public GameObject fighter4;
+    public GameObject fighter5;
+
+    Fighter fighterMasVida;
+    List<Fighter> listaMuertos = new List<Fighter>();
+    List<Fighter> listaLuchadoresRandom = new List<Fighter>();
+
+    //función para crear Lista de Fighters --> todos tendrán la misma vida (health)
+    public List<Fighter> InitFighters(List<string> listaNombres, List<string> listaCategorias, int health)
+    {
+        List<Fighter> listaLuchadores = new List<Fighter>();
+        for (int i = 0; i < listaNombres.Count; i++)
+        {
+            listaLuchadores.Add(new Fighter(listaNombres[i], listaCategorias[i], health));
+        }
+
+        return listaLuchadores;
+    }
+
+
+    //función para crear Lista de Fighters --> cada uno tendrá una vida random entre 80 y 100
+    public List<Fighter> InitFightersRandom(List<string> listaNombres, List<string> listaCategorias, int vidaMinima, int vidaMaxima)
+    {
+
+        List<Fighter> listaLuchadores = new List<Fighter>();
+        for (int i = 0; i < listaNombres.Count; i++)
+        {
+            int healthRandom = Random.Range(vidaMinima, vidaMaxima);
+            listaLuchadores.Add(new Fighter(listaNombres[i], listaCategorias[i], healthRandom));
+        }
+
+        return listaLuchadores;
+    }
+
+
+
+    //función para obtener el Fighter con mayor vida.
+    public Fighter personajeMasVida(List<Fighter> listaLuchadores)
+    {
+
+        int health = 0;
+        foreach (Fighter luchador in listaLuchadores)
+        {
+            if (luchador.health > health)
+            {
+                fighterMasVida = luchador;
+                health = luchador.health;
+            }
+
+        }
+        return fighterMasVida;
+    }
+
+    //función que devuelve la lista con personajes muertos
+    public List<Fighter> Deaths(List<Fighter> listaLuchadores)
+    {
+
+        foreach (Fighter luchador in listaLuchadores)
+        {
+            bool luchadorMuerto = luchador.IsAlive();
+            if (luchadorMuerto == true)
+            {
+                listaMuertos.Add(luchador);
+            }
+        }
+
+        return listaMuertos;
+    }
+
+    //cuando un Fighter notifique que ha muerto, lo anyado a la listaMuertos 
+    public void AnyadirMuerto(Fighter fighter)
+    {
+        listaMuertos.Add(fighter);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        Fighter luchador = fighter.GetComponent<Fighter>();
+
+        List<string> listaNombres = new List<string>();
+        listaNombres.Add("Scorpion");
+        listaNombres.Add("Kano");
+        listaNombres.Add("Sonya");
+        listaNombres.Add("Johnny Cage");
+        listaNombres.Add("Sub-Zero");
+
+        List<string> listaCategorias = new List<string>();
+        listaCategorias.Add("Ninja");
+        listaCategorias.Add("Mercenario");
+        listaCategorias.Add("Teniente");
+        listaCategorias.Add("Actor");
+        listaCategorias.Add("Ninja");
 
 
-        //Debug.Log("[GM] el nombre del luchador es: " + fighter.GetComponent<Fighter>().nombre);
-        //Debug.Log("[GM] la categoría es: "+ fighter.GetComponent<Fighter>().category);
-        //Debug.Log("[GM] la vida es: " + fighter.GetComponent<Fighter>().health);
+        // EJERCICIOS del 4 al 7: crear lista de luchadores llamando a la función InitFighters()
+        //int healthLuchadores = 100;
+        //List<Fighter> listaLuchadores = InitFighters(listaNombres, listaCategorias, healthLuchadores);
+        //foreach (Fighter luchador in listaLuchadores)
+        //{
+        //    Debug.Log(luchador.InfoLuchador());
+        //}
 
 
-        Debug.Log("[GM] el nombre del luchador es: " + luchador.nombre);
-        Debug.Log("[GM] la categoría es: " + luchador.category);
-        Debug.Log("[GM] la vida es: " + luchador.health);
+
+        // EJERCICIOS del 8 al 12: crear lista de luchadores llamando a la función InitFightersRandom(), obtener personaje con más vida
+
+        listaLuchadoresRandom = InitFightersRandom(listaNombres, listaCategorias, 80, 100);
+
+        Debug.Log("-------------------LISTADO DE FIGHTERS-------------------");
+        foreach (Fighter luchador in listaLuchadoresRandom)
+        {
+            Debug.Log(luchador.MuestraInfoLuchador());
+        }
+
+        Debug.Log("El luchador con más vida es -->  " + personajeMasVida(listaLuchadoresRandom).MuestraInfoLuchador());
+
+
+        
+        
+        // OJO!! revisar esto..... y el Update() para ver como notifica el personaje al GO cuando se queda sin vida...
+        listaLuchadoresRandom[1].DecreaseHealth(100);
+
+        Debug.Log("------------LISTADO DE FIGHTERS MUERTOS------------------");
+        
+        foreach (Fighter fighter in listaMuertos)
+        {
+            fighter.MuestraInfoLuchador();
+        }
+
 
     }
+
+
+
+
+
 
     // Update is called once per frame
     void Update()
     {
+
+        foreach (Fighter luchador in listaLuchadoresRandom)
+        {
+            if (luchador.IsAlive()==false) AnyadirMuerto(luchador);
+        }
+
+
 
     }
 }
